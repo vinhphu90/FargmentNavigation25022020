cc742dd12fd08f5819566643836aa2baa073fda7
vinhphu90 <61481544+vinhphu90@users.noreply.github.com>
2020-06-06 20:14:14 +0700

    Naviration

--- app/src/main/java/com/examplevinhphutvp/fargmentnavigation25022020/SplashFragment.java
+++ app/src/main/java/com/examplevinhphutvp/fargmentnavigation25022020/SplashFragment.java
@@ -3,17 +3,36 @@ package com.examplevinhphutvp.fargmentnavigation25022020;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class SplashFragment extends Fragment {
 
+    NavController navController ;
+    View mView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash, container, false);
+        mView = inflater.inflate(R.layout.fragment_splash, container, false);
+        new Handler() . postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //cach 1
+                Navigation.findNavController(getActivity(),R.id.navigationHost)
+                        .navigate(R.id.action_splashFragment_to_loginFragment);
+            }
+        },2000);
+
+        // cách 2
+//        Navigation.findNavController(getActivity(),R.id.navigationHost)
+//                .navigate(R.id.action_splashFragment_to_loginFragment);
+        return mView;
     }
 }
