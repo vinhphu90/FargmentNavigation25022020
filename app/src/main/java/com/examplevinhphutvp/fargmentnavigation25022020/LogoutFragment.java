57776a940c0f49ff237aee3c3e22afc6b8a6d6c9
vinhphu90 <61481544+vinhphu90@users.noreply.github.com>
2020-06-06 20:43:37 +0700

    flow

--- app/src/main/java/com/examplevinhphutvp/fargmentnavigation25022020/LogoutFragment.java
+++ app/src/main/java/com/examplevinhphutvp/fargmentnavigation25022020/LogoutFragment.java
@@ -3,6 +3,7 @@ package com.examplevinhphutvp.fargmentnavigation25022020;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,15 +11,26 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 public class LogoutFragment extends Fragment {
-View mView;
-Button mBtnLogout;
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-         mView=inflater.inflate(R.layout.fragment_logout, container, false);
-         mBtnLogout = mView.findViewById(R.id.buttonLogout);
-        return mView;
+        View mView;
+        Button mBtnLogout;
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            // Inflate the layout for this fragment
+
+            mView = inflater.inflate(R.layout.fragment_logout, container, false);
+            mBtnLogout = mView.findViewById(R.id.buttonLogout);
+
+            mBtnLogout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    NavHostFragment
+                            .findNavController(LogoutFragment.this)
+                            .popBackStack();
+                }
+            });
+
+            return mView;
     }
 }
--- app/src/main/java/com/examplevinhphutvp/fargmentnavigation25022020/forgotPasswordFragment.java
+++ app/src/main/java/com/examplevinhphutvp/fargmentnavigation25022020/forgotPasswordFragment.java
@@ -3,16 +3,44 @@ package com.examplevinhphutvp.fargmentnavigation25022020;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-public class forgotPasswordFragment extends Fragment {
+import android.widget.Button;
+import android.widget.EditText;
 
+public class forgotPasswordFragment extends Fragment {
+    View mView;
+    EditText mEdtEmail;
+    Button mBtnReset,mBtnExit;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_forgot_password, container, false);
+        mView = inflater.inflate(R.layout.fragment_forgot_password, container, false);
+        mEdtEmail = mView.findViewById(R.id.edtEmail);
+        mBtnExit = mView.findViewById(R.id.edtExitForgot);
+        mBtnReset = mView.findViewById(R.id.edtReset);
+
+        mBtnExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NavHostFragment
+                        .findNavController(forgotPasswordFragment.this)
+                        .popBackStack();
+            }
+        });
+
+        mBtnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NavHostFragment
+                        .findNavController(forgotPasswordFragment.this)
+                        .popBackStack();
+            }
+        });
+        return mView;
     }
 }
